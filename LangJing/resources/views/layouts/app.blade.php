565ac0fda4fe7a3d1c278cbed732c4080585a393
XingchenLi <lix17@miamioh.edu>
2016-08-23 21:55:25 +0800

    added new JavaScript and Css file

---
+++ LangJing/resources/views/layouts/app.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/style.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/font-awesome-4.1.0/css/font-awesome.min.css')}}"/>
+
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+</head>
+<body id="app-layout">
+
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Laravel
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="nav">
+        <div class='logo'></div>
+        <ul class="menu">
+            <li class="btMenu">
+                <a href="javascript:void(0)">MENU</a></li>
+            <li class="menuItem"><a href="index.html">Home</a></li>
+            <li class="menuItem"><a href="#">About Us</a></li>
+            <li class="menuItem"><a href="#">Mission</a></li>
+            <li class="menuItem"><a href="#">Features</a></li>
+            <li class="menuItem"><a href="#">Why Us</a></li>
+            <li class="menuItem"><a href="http://www.scriptcafe.in">Download</a></li>
+            <li class="menuItem"><a href="http://www.scriptcafe.in">Documentation</a></li>
+
+        </ul>
+    </div>
+
+
+
+
+    @yield('content')
+
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('js/jquery.js') }}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('js/scrollReveal.js') }}"></script>
+
+    <script type='text/javascript'>
+        jQuery(document).ready(function(){
+
+
+            $(".top").click(function(){
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+            $(".btMenu").click(function(){
+
+                if($(".menu").children(".menuItem").css("display") == "none"){
+                    $(".menu").children(".menuItem").slideDown();
+                }
+                else{
+                    $(".menu").children(".menuItem").slideUp();
+                }
+            });
+            $(window).resize(function(){
+                if($(window).innerWidth() > 900){
+                    $(".menu").children(".menuItem").css("display", "block");
+                }
+                else{
+                    $(".menu").children(".menuItem").css("display", "none");
+                }
+            });
+
+            (function(k){'use strict';k(['jquery'],function($){var j=$.scrollTo=function(a,b,c){return $(window).scrollTo(a,b,c)};j.defaults={axis:'xy',duration:parseFloat($.fn.jquery)>=1.3?0:1,limit:!0};j.window=function(a){return $(window)._scrollable()};$.fn._scrollable=function(){return this.map(function(){var a=this,isWin=!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!isWin)return a;var b=(a.contentWindow||a).document||a.ownerDocument||a;return/webkit/i.test(navigator.userAgent)||b.compatMode=='BackCompat'?b.body:b.documentElement})};$.fn.scrollTo=function(f,g,h){if(typeof g=='object'){h=g;g=0}if(typeof h=='function')h={onAfter:h};if(f=='max')f=9e9;h=$.extend({},j.defaults,h);g=g||h.duration;h.queue=h.queue&&h.axis.length>1;if(h.queue)g/=2;h.offset=both(h.offset);h.over=both(h.over);return this._scrollable().each(function(){if(f==null)return;var d=this,$elem=$(d),targ=f,toff,attr={},win=$elem.is('html,body');switch(typeof targ){case'number':case'string':if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=win?$(targ):$(targ,this);if(!targ.length)return;case'object':if(targ.is||targ.style)toff=(targ=$(targ)).offset()}var e=$.isFunction(h.offset)&&h.offset(d,targ)||h.offset;$.each(h.axis.split(''),function(i,a){var b=a=='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,old=d[key],max=j.max(d,a);if(toff){attr[key]=toff[pos]+(win?0:old-$elem.offset()[pos]);if(h.margin){attr[key]-=parseInt(targ.css('margin'+b))||0;attr[key]-=parseInt(targ.css('border'+b+'Width'))||0}attr[key]+=e[pos]||0;if(h.over[pos])attr[key]+=targ[a=='x'?'width':'height']()*h.over[pos]}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)=='%'?parseFloat(c)/100*max:c}if(h.limit&&/^\d+$/.test(attr[key]))attr[key]=attr[key]<=0?0:Math.min(attr[key],max);if(!i&&h.queue){if(old!=attr[key])animate(h.onAfterFirst);delete attr[key]}});animate(h.onAfter);function animate(a){$elem.animate(attr,g,h.easing,a&&function(){a.call(this,targ,h)})}}).end()};j.max=function(a,b){var c=b=='x'?'Width':'Height',scroll='scroll'+c;if(!$(a).is('html,body'))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,html=a.ownerDocument.documentElement,body=a.ownerDocument.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return $.isFunction(a)||typeof a=='object'?a:{top:a,left:a}}return j})}(typeof define==='function'&&define.amd?define:function(a,b){if(typeof module!=='undefined'&&module.exports){module.exports=b(require('jquery'))}else{b(jQuery)}}));
+
+            $(".menu").children("li:nth-child(3)").click(function(){
+                $("body").scrollTo("#about", 600);
+
+            });
+
+            $(".menu").children("li:nth-child(4)").click(function(){
+                $("body").scrollTo("#mission", 600);
+            });
+
+            $(".menu").children("li:nth-child(5)").click(function(){
+
+                $("body").scrollTo("#whyus", 600);
+            });
+
+            $(".menu").children("li:nth-child(6)").click(function(){
+                $("body").scrollTo("#whychose", 600);
+            });
+
+        });
+
+        (function($) {
+            window.scrollReveal = new scrollReveal();
+        })();
+
+    </script>
+
+
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+</body>
+</html>
